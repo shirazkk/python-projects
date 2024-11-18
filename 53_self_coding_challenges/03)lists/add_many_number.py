8b5ce0436f052b304d22d04282f119d7dd7105b9
Shiraz Ali <shirazkk8@gmail.com>
2024-11-18 15:19:33 +0500

    complete-problem1-of-lists

---
+++ 53_self_coding_challenges/03)lists/add_many_number.py
@@ -0,0 +1,16 @@
+
+"""
+Problem Statement
+
+Write a function that takes a list of numbers and returns the sum of those numbers.
+"""
+
+def sum_of_numbers(numbers: list[int]):
+    return sum(numbers)
+
+
+numbers_list = [1, 2, 3, 4, 5]
+result = sum_of_numbers(numbers_list)
+print(result)
+
+
