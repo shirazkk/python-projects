4421e1b94598c71b1a7193c182487da6ef631d03
Shiraz Ali <shirazkk8@gmail.com>
2024-11-24 21:22:39 +0500

    if/else-problem-01

---
+++ 53_self_coding_challenges/04)if_statements/print_events.py
@@ -0,0 +1,10 @@
+"""
+Write a program that prints the first 20 even numbers. There are several correct approaches, 
+but they all use a loop of some sort. Do no write twenty print statements
+"""
+
+
+print("The first 20 even numbers are:")
+for i in range(0, 20):  
+    even_number = i * 2 
+    print(even_number)
