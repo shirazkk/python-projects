d45a4fed17d5cb6b5fbb1b7e68d1031505d6540e
Shiraz Ali <shirazkk8@gmail.com>
2024-11-27 10:52:42 +0500

    solve/dictionary/problem-count_nums

--- 53_self_coding_challenges/05)dictionaries/count_nums.py
+++ 53_self_coding_challenges/05)dictionaries/count_nums.py
@@ -1,9 +1,18 @@
 
+"""
+Problem Statement
 
-from collections import Counter
+This program counts the number of times each number appears in a list. It uses a dictionary to keep track of the information.
+
+An example run of the program looks like this (user input is in blue):
+
+Enter a number: 3 Enter a number: 4 Enter a number: 3 Enter a number: 6 Enter a number: 4 Enter a number: 3 Enter a number: 12 Enter a number: 3 appears 3 times. 4 appears 2 times. 6 appears 1 times. 12 appears 1 times.
+"""
+
+user_list = []
+num_dict = {}
 
 def get_user_input():
-    user_list = []
     while True:
         try:
             user_input = input("Enter a number: ")
@@ -13,8 +22,13 @@ def get_user_input():
         except ValueError:
             print("Invalid input. Please enter a number.")
 
-    num_counts = Counter(user_list)
-    for num, count in num_counts.items():
-        print(f"{num} appears {count} times.")
+    for num in user_list:
+        if num in num_dict:
+            num_dict[num] += 1
+        else:
+            num_dict[num] = 1
+
+    for num1 in num_dict:
+        print(f"{num1} appears {num_dict[num1]} times.")
 
 get_user_input()
