bd5642fd7448cef72b988830dd919a2a94864516
Shiraz Ali <shirazkk8@gmail.com>
2024-12-03 11:46:19 +0500

    add-question

--- 53_self_coding_challenges/07)functions/averages.py
+++ 53_self_coding_challenges/07)functions/averages.py
@@ -1,11 +1,13 @@
+"""
+Problem Statement
+Write a function that takes two numbers and finds the average between the two.
+"""
+
 def find_average(num1: float, num2: float) -> float:
-    """
-    This function takes two numbers as input and returns their average.
-    """
+    
     average = (num1 + num2) / 2
     return average
 
-# Example usage:
 number1: float = float(input("Enter the first number: "))
 number2: float = float(input("Enter the second number: "))
 
